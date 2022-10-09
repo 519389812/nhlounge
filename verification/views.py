24d541b2dbf0e9781f8b1d6fe3a4f86c7dc6a4e9
519389812 <519389812@qq.com>
2022-10-09 16:54:30 +0800

    upgrate alert

--- verification/views.py
+++ verification/views.py
@@ -18,20 +18,24 @@ def verify(request):
     if request.method == "POST":
         coupon_code = request.POST.get("coupon", "")
         if not coupon_code:
-            return render(request, "verification.html", {"msg_cn": "请输入券码"})
+            messages.error(request, "请输入券码")
+            return redirect(reverse('verification:home'))
         try:
             coupon = Coupon.objects.get(coupon=coupon_code)
         except:
-            return render(request, "verification.html", {"msg_cn": "未找到该券码", "coupon": coupon_code})
+            messages.error(request, "未找到该券码")
+            return redirect(reverse('verification:home'))
         rsa_handler = RsaHandler(coupon.version)
         if not rsa_handler.is_key_exist():
-            return render(request, "verification.html", {"msg_cn": "没有该版本的券码校验文件", "coupon": coupon_code})
+            messages.error(request, "没有该版本的券码校验文件")
+            return redirect(reverse('verification:home'))
         rsa_handler.load_keys()
         result = rsa_handler.verify_text(coupon.coupon, coupon.signature)
         if not result:
             coupon.status = "无效"
             coupon.save()
-            return render(request, "verification.html", {"msg_cn": "校验失败，此券码为无效券码", "coupon": coupon_code})
+            messages.error(request, "校验失败，此券码为无效券码")
+            return redirect(reverse('verification:home'))
         dt = time_zone.localize(datetime.datetime.now())
         if coupon.valid_from_datetime <= dt <= coupon.expiry_datetime:
             if coupon.status == "未使用":
@@ -40,12 +44,15 @@ def verify(request):
                 messages.success(request, '恭喜，兑换成功！')
                 return redirect(reverse('verification:home'))
             else:
-                return render(request, "verification.html", {"msg_cn": "兑换失败！券码不可用，状态为：%s" % coupon.status, "coupon": coupon_code})
+                messages.error(request, "兑换失败！券码不可用，状态为：%s" % coupon.status)
+                return redirect(reverse('verification:home'))
         else:
             if dt < coupon.valid_from_datetime:
-                return render(request, "verification.html", {"msg_cn": "兑换失败！该券码未生效", "coupon": coupon_code})
+                messages.error(request, "兑换失败！该券码未生效")
+                return redirect(reverse('verification:home'))
             elif dt > coupon.expiry_datetime:
-                return render(request, "verification.html", {"msg_cn": "兑换失败！该券码已过期", "coupon": coupon_code})
+                messages.error(request, "兑换失败！该券码已过期")
+                return redirect(reverse('verification:home'))
     else:
         return render(request, "verification.html", {"msg_cn": "请求错误"})
 
